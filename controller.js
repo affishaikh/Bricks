16045a818ab38bd7207657d3b519411e758a7d0d
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-01-12 09:56:16 +0530

    Added functionality to move paddle left and right.

--- controller.js
+++ controller.js
@@ -1,4 +1,7 @@
 const UNIT = 'px';
+const ARROW_RIGHT = 'ArrowRight';
+const ARROW_LEFT = 'ArrowLeft';
+const SPEED = 50;
 
 class Paddle {
   constructor(width, height, left, bottom) {
@@ -6,6 +9,14 @@ class Paddle {
     this.height = height;
     this.left = left;
     this.bottom = bottom;
+  }
+
+  moveRight() {
+    this.left = this.left + SPEED;
+  }
+
+  moveLeft() {
+    this.left = this.left - SPEED;
   }
 }
 
@@ -16,20 +27,40 @@ const setPaddleAttributes = function(paddleDiv, paddle) {
   paddleDiv.style.bottom = paddle.bottom + UNIT;
 };
 
-const drawPaddle = function() {};
+const drawPaddle = function(paddle) {
+  const paddleDiv = document.getElementById('paddleDiv');
+  setPaddleAttributes(paddleDiv, paddle);
+};
+
+const handleKeyEvents = function(paddle, event) {
+  if (event.key === 'ArrowRight') {
+    paddle.moveRight();
+  }
+  if (event.key === 'ArrowLeft') {
+    paddle.moveLeft();
+  }
+  drawPaddle(paddle);
+};
+
+const addEventListener = function(document, paddle) {
+  const world = document.getElementById('world');
+  const keyEventHandler = handleKeyEvents.bind(null, paddle);
+  world.onkeydown = keyEventHandler;
+  world.focus();
+};
 
 const insertPaddle = function(document, paddle) {
   const paddleDiv = document.createElement('div');
   paddleDiv.className = 'paddle';
   paddleDiv.id = 'paddleDiv';
-  setDimensionAndPosition(paddleDiv, paddle);
+  setPaddleAttributes(paddleDiv, paddle);
   document.getElementById('world').appendChild(paddleDiv);
+  addEventListener(document, paddle);
 };
 
 const initialize = function() {
   const paddle = new Paddle(100, 20, 430, 20);
   insertPaddle(document, paddle);
-  drawPaddle();
 };
 
 window.onload = initialize;
