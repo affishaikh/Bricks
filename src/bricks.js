a6a2f50870f67f937d44ae0b9cdaeee7f33eed4f
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-01-13 13:47:45 +0530

    Added ball in the game.
    Ball is moving. It is moving only in one direction.

--- src/bricks.js
+++ src/bricks.js
@@ -1,4 +1,6 @@
 const UNIT = 'px';
+const RIGHT_BORDER = 860;
+const LEFT_BORDER = 0;
 const ARROW_RIGHT = 'ArrowRight';
 const ARROW_LEFT = 'ArrowLeft';
 
@@ -20,10 +22,32 @@ class Paddle {
   }
 }
 
+class Ball {
+  constructor(radius, top, left, topVelocity, leftVelocity) {
+    this.radius = radius;
+    this.top = top;
+    this.left = left;
+    this.topVelocity = topVelocity;
+    this.leftVelocity = leftVelocity;
+  }
+
+  move() {
+    this.top = this.top + this.topVelocity;
+    this.left = this.left + this.leftVelocity;
+  }
+}
+
+class World {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
 const isWithinRightBoundary = function(paddle) {
-  return paddle.left < 860;
+  return paddle.left < RIGHT_BORDER;
 };
 
 const isWithinLeftBoundary = function(paddle) {
-  return paddle.left > 0;
+  return paddle.left > LEFT_BORDER;
 };
--- src/controller.js
+++ src/controller.js
@@ -30,7 +30,6 @@ const addEventListener = function(document, paddle) {
   const world = document.getElementById('world');
   const keyEventHandler = handleKeyEvents.bind(null, paddle);
   world.onkeydown = keyEventHandler;
-  world.focus();
 };
 
 const insertPaddle = function(document, paddle) {
@@ -42,9 +41,63 @@ const insertPaddle = function(document, paddle) {
   addEventListener(document, paddle);
 };
 
+const setWorldAttributes = function(worldDiv, world) {
+  worldDiv.style.width = world.width + UNIT;
+  worldDiv.style.height = world.height + UNIT;
+};
+
+const setFocus = function(document) {
+  const world = document.getElementById('world');
+  world.tabIndex = 0;
+  world.focus();
+};
+
+const insertWorld = function(document, world) {
+  const worldDiv = document.createElement('main');
+  worldDiv.className = 'world';
+  worldDiv.id = 'world';
+  setWorldAttributes(worldDiv, world);
+  document.getElementsByTagName('body')[0].appendChild(worldDiv);
+  setFocus(document);
+};
+
+const setBallAttributes = function(ballDiv, ball) {
+  const ballDiameter = ball.radius * 2;
+  ballDiv.style.width = ballDiameter + UNIT;
+  ballDiv.style.height = ballDiameter + UNIT;
+  ballDiv.style.top = ball.top + UNIT;
+  ballDiv.style.left = ball.left + UNIT;
+  ballDiv.style.borderRadius = ball.radius + UNIT;
+};
+
+const insertBall = function(document, ball) {
+  const ballDiv = document.createElement('div');
+  ballDiv.className = 'ball';
+  ballDiv.id = 'ball';
+  setBallAttributes(ballDiv, ball);
+  document.getElementById('world').appendChild(ballDiv);
+};
+
+const drawBall = function(document, ball) {
+  const ballDiv = document.getElementById('ball');
+  setBallAttributes(ballDiv, ball);
+};
+
+const moveBall = function(document, ball) {
+  setInterval(function() {
+    ball.move();
+    drawBall(document, ball);
+  }, 100);
+};
+
 const initialize = function() {
+  const world = new World(960, 650);
+  insertWorld(document, world);
   const paddle = new Paddle(100, 20, 430, 20);
   insertPaddle(document, paddle);
+  const ball = new Ball(10, 20, 20, 5, 5);
+  insertBall(document, ball);
+  moveBall(document, ball);
 };
 
 window.onload = initialize;
