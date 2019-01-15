9dc7bd0e840485db45475c6bc9b30354c03aef31
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-01-15 17:05:58 +0530

    Modified the tests according to the changes made in src.

--- test/bricksTest.js
+++ test/bricksTest.js
@@ -2,7 +2,7 @@ const { assert } = chai;
 
 describe('paddle.moveLeft', function() {
   it('should decrease the left co-ordinate by 10', function() {
-    const paddle = new Paddle(100, 20, 430, 10);
+    const paddle = new Paddle(100, 20, 610, 430);
     paddle.moveLeft();
     assert.equal(paddle.left, 420);
   });
@@ -10,9 +10,22 @@ describe('paddle.moveLeft', function() {
 
 describe('paddle.moveRight', function() {
   it('should increase the left co-ordinate by 10', function() {
-    const paddle = new Paddle(100, 20, 430, 10);
+    const paddle = new Paddle(100, 20, 610, 430);
     paddle.moveRight();
     assert.equal(paddle.left, 440);
+  });
+});
+
+describe('paddle.didBallCollide', function() {
+  it('should return true when ball collides paddle', function() {
+    const paddle = new Paddle(100, 20, 610, 430);
+    const ball = new Ball(10, 590, 430, 10, 10);
+    const actualOutput = paddle.didBallCollide(
+      ball.top,
+      ball.left,
+      ball.radius
+    );
+    assert.equal(actualOutput, true);
   });
 });
 
@@ -24,14 +37,5 @@ describe('ball.move', function() {
     ball.move();
     const actualOutput = new Ball(10, 30, 30, 10, 10);
     assert.deepEqual(ball, actualOutput);
-  });
-});
-
-describe('ball.didCollideWithPaddle', function() {
-  it('should return true when ball collides paddle', function() {
-    const ball = new Ball(10, 590, 430, 10, 10);
-    const paddle = new Paddle(100, 20, 430, 610 , 10);
-    const actualOutput = ball.didCollideWithPaddle(paddle);
-    assert.equal(actualOutput, true);
   });
 });
